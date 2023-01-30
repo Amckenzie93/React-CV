5e22b83cff4c40fa878eb5e59eb0ccd882c0ae0b
Adam McKenzie <AdamM@forrit.com>
2023-01-30 16:48:03 +0000

    content update

--- src/App.js
+++ src/App.js
@@ -62,8 +62,8 @@ class App extends Component {
                     <main className="col">
                         <TextBody heading={"Bio"}
                             text={[
-                                "With almost 10 years’ commercial experience working within the web industry and a first class honours degree in software development, i have taken on a new challenge in becoming a lead front end developer, applying the knowledge i have soaked up over the years including Responsive Web Development, Accessibility, Web interactions and animation, and many more. I enjoy the ability to do so whilst keeping and maintaining a clean codebase utilising modern tech stacks.",
-                                "One of my favorite aspects in what i do, and what i consider myself lucky in, is being able to offer help and support to others in my field - i take no greater joy and am honoured by the fact i can help those in areas i struggled to grasp over the years, and find myself having a meaningful impact on peoples careers."
+                                "With almost 10 years’ commercial experience working within the web industry and a first class honours degree in software development, I have taken on a new challenge in becoming a lead front end developer, applying the knowledge I have soaked up over the years including Responsive Web Development, Accessibility, Web interactions and animation, and many more. I enjoy the ability to do so whilst keeping and maintaining a clean codebase utilising modern tech stacks.",
+                                "One of my favorite aspects in what I do, and what I consider myself lucky in, is being able to offer help and support to others in my field - I take no greater joy and am honoured by the fact I can help those in areas I struggled to grasp over the years, and find myself having a meaningful impact on peoples careers."
                             ]}
 
                         />
@@ -94,9 +94,9 @@ class App extends Component {
                                     ariaLabel: "Follow this link to learn more about Forrit.",
                                     year: "2021 - present",
                                     position: "Lead Front End Developer",
-                                    text: ["During the last two years at Forrit ltd, i was promoted as the Lead Front end developer. This has been such an exciting pivotol change bringing a whole myriad of new challenges and opertunities.",
+                                    text: ["During the last two years at Forrit ltd, I was promoted as the Lead Front end developer. This has been such an exciting pivotol change bringing a whole myriad of new challenges and opertunities.",
                                         "explain whats new ... to be written",
-                                        "Recently i built Forrits new public facing website, i encourage you to please do check it out! I used LESS css along with typescript, gulp, and many other tools along side our own inhouse built CMS platform 'Forrit'."
+                                        "Recently I built Forrits new public facing website, I encourage you to please do check it out! I used LESS css along with typescript, gulp, and many other tools along side our own inhouse built CMS platform 'Forrit'."
                                     ]
                                 },
                                 {
@@ -107,8 +107,9 @@ class App extends Component {
                                     ariaLabel: "Follow this link to learn more about Forrit.",
                                     year: "2016 - 2021",
                                     position: "Senior Developer",
-                                    text: ["As a senior developer within the Microsoft delivery team at Forrit, I perform many duties such as handling large website migrations, creating and maintaining responsive web pages, building accessible yet interactive user interfaces and features for all to use. I also maintain the integrity of the project code base alongside managing deployments of new updates to the public website",
-                                        "maintaining responsive web pages, building accessible yet interactive user interfaces and features for all to use. I also maintain the integrity of the project code base alongside managing deployments of new updates to the public website."]
+                                    text: ["As a Senior Web Developer at Forrit, I worked with multiple large scale household name clients, such as Microsoft Education, Lloyds of London, Macallan, Motorola, and more. Over the years I sucssessfully delivered several full scale website migrations into our in house solutions. Many projects required new functionality over time, from small to large component updates, to complete redesigns of client websites. "
+                                        , "Collaborating with clients and colleagues was a vital part of my work, ensuring my knowledge was applied to help improve the performance and quality of the work being analysed. This often lead to smoother and more sucsessful outcomes for the client. "
+                                        , "My role covered mny aspects from creating and maintaining responsive web pages, building accessible yet interactive user interfaces and features for all to use to WCAG standards, utilising differing front end frameworks best suited to the projects needs. I maintained the integrity of several project code bases, alongside managing deployments of new updates to clients public facing websites. I have developed several in house toolkit applications  at Forrit which aid across the service delivery teams in day to day functions. "]
                                 },
                                 {
                                     Company: "Inforgen",
